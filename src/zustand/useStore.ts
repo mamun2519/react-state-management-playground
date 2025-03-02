f9a0898412889ca4060b7f4423a8d2586049ce5e
mamun2519 <juborajislam46@gmail.com>
2025-03-02 23:32:18 +0600

    feat: added to zustand fetures

--- src/zustand/useStore.ts
+++ src/zustand/useStore.ts
@@ -4,3 +4,18 @@ type Store = {
   user: User | null;
   demoString: string;
 };
+
+type Action = {
+  logIn: (user: User) => void;
+  logOut: () => void;
+  updateDemoString: (demoString: string) => void;
+};
+
+const useStore = create<Store & Action>((set) => ({
+  user: null,
+  demoString: "demo",
+  logIn: (user) => set({ user }),
+  logOut: () => set({ user: null }),
+}));
+
+export default useStore;
