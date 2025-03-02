759b3835f88a954f6846c63c0852d17a8ac4cd77
mamun2519 <juborajislam46@gmail.com>
2025-03-02 23:46:12 +0600

    feat: explore react tool kit advance fetures

--- src/redux-toolkit/useStore.ts
+++ src/redux-toolkit/useStore.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { User } from "../type";
+import { TypedUseSelectorHook, useDispatch } from "react-redux";
 
 interface UserState {
   user: User | null;
@@ -32,3 +33,18 @@ const userSlice = createSlice({
     },
   },
 });
+
+const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Export typed versions of hooks
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
