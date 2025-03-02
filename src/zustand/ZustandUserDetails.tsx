4065972c943b1e002c3fa2ea5a195c026569fd2f
mamun2519 <juborajislam46@gmail.com>
2025-03-02 23:37:42 +0600

    feat: added to zustand fetures

--- src/zustand/ZustandUserDetails.tsx
+++ src/zustand/ZustandUserDetails.tsx
@@ -5,6 +5,16 @@ const ZustandUserDetails = () => {
   const user = useUserStore((state) => state.user);
   const logOut = useUserStore((state) => state.logOut);
   const logIn = useUserStore((state) => state.logIn);
+
+  const updateDemoString = useUserStore((state) => state.updateDemoString);
+
+  const handleLogin = async (userId: string) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    const user = await response.json();
+    logIn(user);
+  };
   return (
     <div>
       <h1>User Details</h1>
@@ -12,6 +22,16 @@ const ZustandUserDetails = () => {
       <p>Email: {user?.email}</p>
       <p>Phone: {user?.phone}</p>
       <p>Role: {user?.role}</p>
+
+      <br />
+
+      <button onClick={() => handleLogin("1")}>Log In</button>
+      <button onClick={logOut}>Log Out</button>
+      <br />
+
+      <button onClick={() => updateDemoString("Hello world" + Math.random())}>
+        Update Demo String
+      </button>
     </div>
   );
 };
