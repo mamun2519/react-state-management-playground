de054541d90edef8882d427f702841c02f4a706d
mamun2519 <juborajislam46@gmail.com>
2025-03-02 23:43:50 +0600

    feat: explore react tool kit advance fetures

--- src/redux-toolkit/useStore.ts
+++ src/redux-toolkit/useStore.ts
@@ -4,3 +4,14 @@ interface UserState {
   user: User | null;
   demoString: string;
 }
+
+interface UserAction {
+  logIn: (user: User) => void;
+  logOut: () => void;
+  updateDemoString: (demoString: string) => void;
+}
+
+const initialState: UserState = {
+  user: null,
+  demoString: "demo",
+};
