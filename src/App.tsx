66fa42d76e56a4403c20bf1437ef9a0271a24c67
mamun2519 <juborajislam46@gmail.com>
2025-03-02 23:50:05 +0600

    feat: explore react tool kit advance fetures

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import ReduxUserDetails from "./redux-toolkit/ReduxUserDetails";
+import { ReduxWrapper } from "./redux-toolkit/ReduxWrapper";
 import { ZustandDemoString } from "./zustand/ZustandDemoString";
 import ZustandUserDetails from "./zustand/ZustandUserDetails";
 
 function App() {
   return (
     <>
+      {/* Zustand */}
       <ZustandUserDetails />
       <ZustandDemoString />
+
+      {/* Redux */}
+      <ReduxWrapper>
+        <ReduxUserDetails />
+      </ReduxWrapper>
     </>
   );
 }
